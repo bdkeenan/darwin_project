b0ad66c20f712a65498548397fe98dbefb6c48a6
bdkeenan <bdk@ku.edu>
2013-06-24 13:16:52 -0500

    None.

---
+++ darwin_funcs.v1.cpp
@@ -0,0 +1,198 @@
+/*
+This is an anyD Darwin eom function with (fixed step) rk4 solver, for N particles.
+
+Here, the C++ stl "valarray" is used to store the particle data. This container
+has a few advantages. First, like a "vector", it deals with the dynamic memory allocation automatically, so
+we don't have to worry about memory leaks. Second, valarrays are defined with several operator overloads that
+are particularly useful (e.g. vector-scalar multiplication, and vector addition).
+
+I have implemented valarrays of dimension one. They actually signify 2D arrays:
+(Number of particles) /times (N coordinates + N momenta). I'm using what's called, "row-major order". With
+this, a 2D (dimensions: rows*col) array is represented as a 1D array using the following transformation:
+
+array2d[i][j] --> array1d[i*col + j].
+
+In our case, col = (two /times number of spatial dimensions), and the number of rows is the (total number of particles).
+
+While this is confusing at first, it does provide a considerable advantage. For one, these arrays are easier
+to work with. Additionally, array elements that are continguous in memory can be accessed faster this way,
+because of lack of caching.
+
+Finally, I've defined these functions as "inline". This MIGHT make them faster ... but it's hard to say, really.
+
+Anyway, I recommend that we try this out (see if it works), and then give a little more thought to
+optimization.
+*/
+
+#include <iostream>
+//valarrays require:
+#include <valarray>
+
+
+using namespace std;
+
+/*
+rk4 function prototype, where "r" is a valarray containing the x, y, ... and px, py, ... for each particle.
+Use row-major ordering. "h" is the time step size, "t" is the current time, and "dim" is the number of
+spatial dimensions.
+*/
+void inline rk4(valarray<double> &r, double h, double &t, const int dim);
+/*
+EOM prototype. "k" is a valarray which contains the derivatives used in each step of rk4.
+*/
+void inline func(valarray<double> &r, valarray<double> &k, const int dim);
+
+
+int main()
+
+{
+    //I.C.s, loops, etc.
+
+
+    return 0;
+
+}
+
+
+
+void inline rk4(valarray<double> &r, double h, double &t, const int dim)
+
+{
+    //define k, r_new, and temp. arrays to hold eom evaluations
+    valarray<double> k(r.size());
+    valarray<double> temp(r.size());
+    valarray<double> r_new(r.size());
+
+    double half_h = h / 2.;
+
+    //1st rk4 step
+    func(r, k, dim);
+    r_new = h * (1. / 6.) * k;
+    temp = r + half_h * k;
+
+    //2nd
+    func(temp, k, dim);
+    r_new += h * (1. / 3.) * k;
+    temp = r + half_h * k;
+
+    //3rd
+    func(temp, k, dim);
+    r_new += h * (1. / 3.) * k;
+    temp = r + h * k;
+
+    //4th
+    func(temp, k, dim);
+
+    //advance r in time
+    r = r_new + h * (1. / 6.) * k;
+
+    //advance time
+    t += h;
+}
+
+
+void inline func(valarray<double> &r, valarray<double> &k, const int dim)
+
+{
+    //constants
+    const double m = 1;
+    const double c = 1;
+    const double e = -1;
+    const double fac = 1. / (2.*m * m * m * c * c);
+    const double fac1 = 1. / (2.*m * m * c * c);
+
+
+    //reset k vector
+    for (unsigned int i = 0; i < k.size(); i++)
+    {
+        k[i] = 0.0;
+    }
+
+    //initialize vector to contain n_ij values
+    double *n_ij = new double[dim];
+
+    //get number of particles
+    const int N_p = r.size() / dim;
+
+    //arrays arrive in 1D form, where an element is identified by: i_par * dim + j_dim
+
+    for (int i = 0; i < N_p; i++)
+    {
+
+        //calculate p_i^2
+        double p_sq = 0.0;
+
+        for (int d = 0; d < dim; d++)
+
+        {
+            p_sq += r[(i + 1) * dim + d] * r[(i + 1) * dim + d];
+        }
+
+        //evaluate equations of motion: -dH/dx = dp/dt; dH/dp = dx/dt
+        for (int d = 0; d < dim; d++)
+        {
+            //dH/dp, 1st part
+            k[i * dim + d] = r[(i + 1) * dim + d] / m - p_sq * fac * r[(i + 1) * dim + d];
+        }
+
+        //sum part of eom evaluation. Note: this probably needs a more efficient implementation
+        for (int j = 0; j < N_p; j++)
+        {
+            double R_ij = 0.0;
+
+            double pjdotn = 0.0;
+            double pidotn = 0.0;
+            double pidotpj = 0.0;
+
+
+            //don't count ith particle
+            if (i != j)
+            {
+                //calculate R_ij
+                for (int d = 0; d < dim; d++)
+
+                {
+                    R_ij += (r[i * dim + d] - r[j * dim + d]) * (r[i * dim + d] - r[j * dim + d]);
+                }
+
+                R_ij = sqrt(R_ij);
+
+                //calculate n_ij[d]
+                for (int d = 0; d < dim; d++)
+
+                {
+                    n_ij[d] = (r[i * dim + d] - r[j * dim + d]) / R_ij;
+                }
+
+                //calculate some dot products
+                for (int d = 0; d < dim; d++)
+
+                {
+                    pjdotn += n_ij[d] * r[(j + 1) * dim + d];
+                    pidotn += n_ij[d] * r[(i + 1) * dim + d];
+                    pidotpj += r[(i + 1) * dim + d] * r[(j + 1) * dim + d];
+                }
+
+                for (int d = 0; d < dim; d++)
+
+                {
+                    //rest of dH/dp
+                    k[i * dim + d] -= (fac / R_ij) * (r[(j + 1) * dim + d] + pjdotn * n_ij[d]);
+
+                    //-dH/dx
+                    double A = (e * e) / (R_ij * R_ij);
+                    k[(i + 1)*dim + d] += A * n_ij[d] * (1. - fac1 * pidotpj) - 3.*fac1 * A * n_ij[d] * pidotn * pjdotn;
+                    k[(i + 1)*dim + d] += fac1 * A * (r[(i + 1) * dim + d] * pjdotn + r[(j + 1) * dim + d] * pidotn);
+                }
+
+            }
+
+
+        }
+
+    }
+
+    //free n_ij
+    delete [] n_ij;
+
+}
